18a939191f43747846a4924253d4a7620fa68920
Tyler Segovia <47987889+Termina1Zer0@users.noreply.github.com>
2019-03-26 17:48:05 -0300

    StarterFile
    The starter file similar to in-class demo

---
+++ Lab5_TextEditor/Lab5_TextEditor/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TextEditorForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.mnuEditorOptions = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuNew = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuSaveAs = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClose = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEdit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuCopy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuCut = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuPaste = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHelp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuAbout = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.txtEditText = New System.Windows.Forms.TextBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.toolTipTextEditor = New System.Windows.Forms.ToolTip(Me.components)
+        Me.mnuEditorOptions.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'mnuEditorOptions
+        '
+        Me.mnuEditorOptions.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.mnuEditorOptions.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuEdit, Me.mnuHelp})
+        Me.mnuEditorOptions.Location = New System.Drawing.Point(0, 0)
+        Me.mnuEditorOptions.Name = "mnuEditorOptions"
+        Me.mnuEditorOptions.Size = New System.Drawing.Size(782, 28)
+        Me.mnuEditorOptions.TabIndex = 1
+        Me.mnuEditorOptions.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuNew, Me.mnuOpen, Me.mnuSave, Me.mnuSaveAs, Me.mnuClose, Me.mnuExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(44, 24)
+        Me.mnuFile.Text = "&File"
+        '
+        'mnuNew
+        '
+        Me.mnuNew.Name = "mnuNew"
+        Me.mnuNew.Size = New System.Drawing.Size(196, 26)
+        Me.mnuNew.Text = "&New        Ctrl+N"
+        '
+        'mnuOpen
+        '
+        Me.mnuOpen.Name = "mnuOpen"
+        Me.mnuOpen.Size = New System.Drawing.Size(196, 26)
+        Me.mnuOpen.Text = "&Open        Ctrl+O"
+        '
+        'mnuSave
+        '
+        Me.mnuSave.Name = "mnuSave"
+        Me.mnuSave.Size = New System.Drawing.Size(196, 26)
+        Me.mnuSave.Text = "&Save        Ctrl+S"
+        '
+        'mnuSaveAs
+        '
+        Me.mnuSaveAs.Name = "mnuSaveAs"
+        Me.mnuSaveAs.Size = New System.Drawing.Size(196, 26)
+        Me.mnuSaveAs.Text = "Save &As"
+        '
+        'mnuClose
+        '
+        Me.mnuClose.Name = "mnuClose"
+        Me.mnuClose.Size = New System.Drawing.Size(196, 26)
+        Me.mnuClose.Text = "&Close"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(196, 26)
+        Me.mnuExit.Text = "E&xit"
+        '
+        'mnuEdit
+        '
+        Me.mnuEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuCopy, Me.mnuCut, Me.mnuPaste})
+        Me.mnuEdit.Name = "mnuEdit"
+        Me.mnuEdit.Size = New System.Drawing.Size(47, 24)
+        Me.mnuEdit.Text = "&Edit"
+        '
+        'mnuCopy
+        '
+        Me.mnuCopy.Name = "mnuCopy"
+        Me.mnuCopy.Size = New System.Drawing.Size(192, 26)
+        Me.mnuCopy.Text = "&Copy        Ctrl+C"
+        '
+        'mnuCut
+        '
+        Me.mnuCut.Name = "mnuCut"
+        Me.mnuCut.Size = New System.Drawing.Size(192, 26)
+        Me.mnuCut.Text = "C&ut        Ctrl+X"
+        '
+        'mnuPaste
+        '
+        Me.mnuPaste.Name = "mnuPaste"
+        Me.mnuPaste.Size = New System.Drawing.Size(192, 26)
+        Me.mnuPaste.Text = "&Paste        Ctrl+V"
+        '
+        'mnuHelp
+        '
+        Me.mnuHelp.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuAbout})
+        Me.mnuHelp.Name = "mnuHelp"
+        Me.mnuHelp.Size = New System.Drawing.Size(53, 24)
+        Me.mnuHelp.Text = "&Help"
+        '
+        'mnuAbout
+        '
+        Me.mnuAbout.Name = "mnuAbout"
+        Me.mnuAbout.Size = New System.Drawing.Size(125, 26)
+        Me.mnuAbout.Text = "&About"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'txtEditText
+        '
+        Me.txtEditText.Location = New System.Drawing.Point(15, 46)
+        Me.txtEditText.Multiline = True
+        Me.txtEditText.Name = "txtEditText"
+        Me.txtEditText.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtEditText.Size = New System.Drawing.Size(755, 391)
+        Me.txtEditText.TabIndex = 2
+        Me.toolTipTextEditor.SetToolTip(Me.txtEditText, "Add, delete or edit text here.")
+        '
+        'TextEditorForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(782, 450)
+        Me.Controls.Add(Me.txtEditText)
+        Me.Controls.Add(Me.mnuEditorOptions)
+        Me.Name = "TextEditorForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Text Editor"
+        Me.mnuEditorOptions.ResumeLayout(False)
+        Me.mnuEditorOptions.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents mnuEditorOptions As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents mnuOpen As ToolStripMenuItem
+    Friend WithEvents mnuSave As ToolStripMenuItem
+    Friend WithEvents txtEditText As TextBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents mnuEdit As ToolStripMenuItem
+    Friend WithEvents mnuHelp As ToolStripMenuItem
+    Friend WithEvents mnuNew As ToolStripMenuItem
+    Friend WithEvents mnuSaveAs As ToolStripMenuItem
+    Friend WithEvents mnuClose As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents mnuCopy As ToolStripMenuItem
+    Friend WithEvents mnuCut As ToolStripMenuItem
+    Friend WithEvents mnuPaste As ToolStripMenuItem
+    Friend WithEvents mnuAbout As ToolStripMenuItem
+    Friend WithEvents toolTipTextEditor As ToolTip
+End Class
---
+++ Lab5_TextEditor/Lab5_TextEditor/Form1.vb
@@ -0,0 +1,37 @@
+﻿Option Strict On
+Imports System.IO
+
+Public Class TextEditorForm
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        Application.Exit()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles mnuOpen.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Try
+                Dim reader As New StreamReader(OpenFileDialog1.FileName)
+                txtEditText.Text() = reader.ReadToEnd()
+                reader.Close()
+            Catch ex As Exception
+                Console.WriteLine(ex.ToString())
+            End Try
+        End If
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles mnuSave.Click
+        SaveFileDialog1.Filter = "TXT Files (*.txt)|*.txt"
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Try
+                My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, txtEditText.Text(), False)
+            Catch ex As Exception
+                Console.WriteLine(ex.ToString())
+            End Try
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        Application.Exit()
+    End Sub
+
+End Class
